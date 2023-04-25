088577c315247d65e73700c10cac8b5e50196b4b
bulk <harryhamlin@gmail.com>
2023-04-24 20:57:37 -0700

    adjusted homepage grid slightly

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,20 +4,29 @@ import { Col, Row } from 'antd';
 
 const Home = () => {
   return <div>
-    <Row gutter={[32, 16]}>
-      <Col span={8}>
+    <Row gutter={[32, 16]} justify={'center'}>
+      <Col span={6}>
         <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
       </Col>
-      <Col span={8}>
+      <Col span={6}>
         <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
       </Col>
-      <Col span={8}>
+      <Col span={6}>
         <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
       </Col>
-      <Col span={8}>
+      <Col span={6}>
         <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
       </Col>
-      <Col span={8}>
+      <Col span={6}>
+        <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
+      </Col>
+      <Col span={6}>
+        <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
+      </Col>
+      <Col span={6}>
+        <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
+      </Col>
+      <Col span={6}>
         <ProductItem title='title' description='description' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' alt='example' />
       </Col>
     </Row>
