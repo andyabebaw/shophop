82055a5d24bbd3e5b0cd7a7caf46d2c7c26a541c
Andrew Chou <andrewchou1996@gmail.com>
2023-04-26 19:29:31 -0700

    reformat

--- client/src/components/CartIcon/index.js
+++ client/src/components/CartIcon/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { FloatButton } from 'antd';
-import { ShoppingCartOutlined } from '@ant-design/icons'
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { FloatButton } from "antd";
+import React from "react";
 
-const CartIcon = () => <FloatButton type={'primary'} icon={<ShoppingCartOutlined />} shape={'square'} onClick={() => console.log('click')} />;
+const CartIcon = () => (
+  <FloatButton
+    type={"primary"}
+    icon={<ShoppingCartOutlined />}
+    shape={"square"}
+    onClick={() => console.log("click")}
+  />
+);
 
 export default CartIcon;
