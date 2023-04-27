f27bdffda423c6a5f123460858281552a3dbe738
Andrew Chou <andrewchou1996@gmail.com>
2023-04-27 08:25:40 -0700

    Storecontext and filter search bar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { QUERY_ALL_PRODUCTS, QUERY_CATEGORIES } from "../utils/queries";
 // import ProductItem from '../components/ProductItem';
 import { Col, Row, Space } from "antd";
@@ -8,106 +8,119 @@ import ProductCard from "../components/ProductCard";
 // import Cart from "../components/Cart";
 
 const Home = () => {
-  // const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+    console.log("loading", loading);
+    console.log("product data", data);
+  }, [data, loading]);
 
-  // useEffect(() => {
-  //   if (data) {
-  //     console.log(data);
-  //   }
-  //   console.log("loading", loading);
-  //   console.log("product data", data);
-  // }, [data, loading]);
-
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // return (
-  //   <>
-  //     <div>Homepage</div>
-  //     <hr />
-  //     {data?.products?.map((d) => (
-  //       <div key={d._id}>
-  //         <div>{d._id}</div>
-  //         <div>{d.name}</div>
-  //       </div>
-  //     ))}
-  //   </>
-  // );
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <Row gutter={[16, 32]} justify={"center"}>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-      <Col span={6}>
-        <ProductCard
-          name="hi"
-          price={22}
-          image={
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          }
-          _id={1}
-        />
-      </Col>
-    </Row>
+    <>
+      <div>Homepage</div>
+      <hr />
+      <input
+        placeholder="search by categories"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <hr />
+      {data?.products
+        ?.filter((d) => {
+          return search.toLocaleLowerCase() === ""
+            ? d
+            : d.categories.some((category) =>
+                category.name.toLowerCase().includes(search.toLowerCase())
+              );
+        })
+        .map((d) => (
+          <div key={d._id}>
+            <div>${d.price}</div>
+            <div>{d.name}</div>
+          </div>
+        ))}
+    </>
   );
+
+  // return (
+  //   <Row gutter={[16, 32]} justify={"center"}>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //     <Col span={6}>
+  //       <ProductCard
+  //         name="hi"
+  //         price={22}
+  //         image={
+  //           "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  //         }
+  //         _id={1}
+  //       />
+  //     </Col>
+  //   </Row>
+  // );
 };
 export default Home;
