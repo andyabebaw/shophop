dc2891fb8d08e8fe463e45eca7f93fc5fdf61559
bulk <harryhamlin@gmail.com>
2023-04-29 09:16:17 -0700

    finished addproduct form

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,7 +1,25 @@
+// import React from "react";
+// import CreateProduct from "../components/ProductEditForm";
+// import ProductLIst from "../components/ProductList"
+
+// const EditProduct = () => {
+//     return (
+//         <div>
+//             <CreateProduct />
+//             {/* <ProductLIst /> */}
+//         </div>
+//     )
+// }
+
+// export default EditProduct;
+
 import { useMutation } from "@apollo/client";
 import { useState } from "react";
 import { ADD_PRODUCT } from "../utils/mutations";
-import "./admin.css";
+import { Form, Input, Button, Upload, InputNumber } from 'antd';
+import {
+  PlusOutlined
+} from "@ant-design/icons";
 
 const Admin = () => {
   const [productData, setProductData] = useState({
@@ -34,6 +52,11 @@ const Admin = () => {
     }
   };
 
+  const handleNameInputChange = (event) => {
+    const { name, value } = event.target
+    setProductData({ ...productData, [name]: parseFloat(value) });
+  }
+
   const handleCategoryInputChange = (event) => {
     const { value } = event.target;
     setProductData({
@@ -46,90 +69,189 @@ const Admin = () => {
     setProductData({ ...productData, categories: [] });
   };
 
-  const handleSubmit = async (event) => {
-    console.log("productData in the form", productData);
-    event.preventDefault();
+  const handleSubmit = async (values) => {
+    console.log("productData in the form", values);
+    // event.preventDefault();
 
     try {
       const mutationResponse = await addProduct({
         variables: {
-          product: productData,
+          product: values,
         },
       });
 
       console.log(mutationResponse);
     } catch (err) {
+      console.log('this is an error')
       console.error(err);
     }
   };
 
+  // return (
+  //   <form onSubmit={handleSubmit}>
+  //     <div>
+  //       <label htmlFor="name">Name:</label>
+  //       <input
+  //         type="text"
+  //         id="name"
+  //         name="name"
+  //         value={productData.name}
+  //         onChange={handleInputChange}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="price">Price:</label>
+  //       <input
+  //         type="number"
+  //         id="price"
+  //         name="price"
+  //         value={productData.price}
+  //         onChange={handleInputChange}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="quantity">Quantity:</label>
+  //       <input
+  //         type="number"
+  //         id="quantity"
+  //         name="quantity"
+  //         value={productData.quantity}
+  //         onChange={handleInputChange}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="description">Description:</label>
+  //       <textarea
+  //         id="description"
+  //         name="description"
+  //         value={productData.description}
+  //         onChange={handleInputChange}
+  //       ></textarea>
+  //     </div>
+  //     <div>
+  //       <label htmlFor="image">Image:</label>
+  //       <input
+  //         type="text"
+  //         id="image"
+  //         name="image"
+  //         value={productData.image}
+  //         onChange={handleInputChange}
+  //       />
+  //     </div>
+  //     <div className="categories">
+  //       <label htmlFor="categories">Categories:</label>
+  //       <input
+  //         type="text"
+  //         id="categories"
+  //         name="categories"
+  //         value={productData.categories.join(", ")}
+  //         onChange={handleCategoryInputChange}
+  //       />
+  //       <button type="button" onClick={handleClearCategories}>
+  //         Clear Categories
+  //       </button>
+  //     </div>
+  //     <button type="submit">Add Product</button>
+  //   </form>
+  // );
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={productData.name}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="price">Price:</label>
-        <input
-          type="number"
-          id="price"
-          name="price"
-          value={productData.price}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="quantity">Quantity:</label>
-        <input
-          type="number"
-          id="quantity"
-          name="quantity"
-          value={productData.quantity}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          name="description"
-          value={productData.description}
-          onChange={handleInputChange}
-        ></textarea>
-      </div>
-      <div>
-        <label htmlFor="image">Image:</label>
-        <input
-          type="text"
-          id="image"
-          name="image"
-          value={productData.image}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="categories">
-        <label htmlFor="categories">Categories:</label>
-        <input
-          type="text"
-          id="categories"
-          name="categories"
-          value={productData.categories.join(", ")}
-          onChange={handleCategoryInputChange}
-        />
-        <button type="button" onClick={handleClearCategories}>
-          Clear Categories
-        </button>
-      </div>
-      <button type="submit">Add Product</button>
-    </form>
-  );
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{
+        categories: [],
+        image: ""
+      }}
+      onFinish={(values) => {
+        handleSubmit(values)
+        // console.log(values)
+      }}
+      // onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+
+      <Form.Item
+        label="Product Name"
+        name="name"
+
+        // onChange={handleNameInputChange}
+        rules={[{ required: true, message: 'Product name Required' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Product Description"
+        name="description"
+        // value={productData.price}
+        // onChange={handleInputChange}
+        rules={[{ required: true, message: 'Product Description Required' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      {/* <Form.Item label="image" name="image" valuePropName="fileList" getValueFromEvent={normFile}> */}
+      {/* <Form.Item label="Image" name="image" valuePropName="fileList" value="">
+        <Upload action="/upload.do" listType="picture-card">
+          <div>
+            <PlusOutlined />
+            <div style={{ marginTop: 8 }}>Upload</div>
+          </div>
+        </Upload>
+      </Form.Item> */}
+
+      <Form.Item
+        label="Image"
+        name="image"
+      // onChange={handleCategoryInputChange}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Quantity"
+        name="quantity"
+        rules={[{ required: true, message: 'Quantity Required' }]}
+      // value={productData.quantity}
+      // onChange={handleInputChange}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label="Price"
+        name="price"
+        rules={[{ required: true, message: 'Quantity Required' }]}
+      // value={productData.price}
+      // onChange={handleInputChange}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label="Category"
+        name="categories"
+      // onChange={handleCategoryInputChange}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Add Product
+        </Button>
+      </Form.Item>
+    </Form>
+  )
 };
 
 export default Admin;
